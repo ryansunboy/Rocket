c931bd05bb0318c2f38fe7615e9d89828bb00b73
liuyang20270 <393356237@qq.com>
2018-05-27 19:05:43 +0800

    Roket2.1 base function module commit

---
+++ rocket-example/src/main/java/org/snail/rocket/App.java
@@ -0,0 +1,13 @@
+package org.snail.rocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
