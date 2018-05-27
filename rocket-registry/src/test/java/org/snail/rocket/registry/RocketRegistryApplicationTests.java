85b8cbdeab3c2b0febe0b7e3ea916fc705d86277
liuyang20270 <393356237@qq.com>
2018-05-27 19:11:05 +0800

    Roket2.1 registry function module commit

---
+++ rocket-registry/src/test/java/org/snail/rocket/registry/RocketRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package org.snail.rocket.registry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RocketRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
